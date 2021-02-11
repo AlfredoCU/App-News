3c0a0df8f4f60518cab189ad73cdce6841499085
AlfredoCU <alfredo_11_cu@hotmail.com>
2021-02-11 00:58:20 -0600

    Se creo un listado de ciudades y categorias que coinciden con la api

---
+++ src/helpers/categories.js
@@ -0,0 +1,30 @@
+export const categories = [
+  {
+    Value: "business",
+    Label: "Negocios"
+  },
+  {
+    Value: "entertainment",
+    Label: "Entretenimiento"
+  },
+  {
+    Value: "general",
+    Label: "General"
+  },
+  {
+    Value: "health",
+    Label: "Salud"
+  },
+  {
+    Value: "science",
+    Label: "Ciencia"
+  },
+  {
+    Value: "sports",
+    Label: "Deportes"
+  },
+  {
+    Value: "technology",
+    Label: "Tecnología"
+  }
+];
---
+++ src/helpers/countries.js
@@ -0,0 +1,218 @@
+export const countries = [
+  {
+    Country: "United Arab Emirates",
+    ISO2: "AE"
+  },
+  {
+    Country: "Argentina",
+    ISO2: "AR"
+  },
+  {
+    Country: "Austria",
+    ISO2: "AT"
+  },
+  {
+    Country: "Australia",
+    ISO2: "AU"
+  },
+  {
+    Country: "Belgium",
+    ISO2: "BE"
+  },
+  {
+    Country: "Bulgaria",
+    ISO2: "BG"
+  },
+  {
+    Country: "Brazil",
+    ISO2: "BR"
+  },
+  {
+    Country: "Canada",
+    ISO2: "CA"
+  },
+  {
+    Country: "Switzerland",
+    ISO2: "CH"
+  },
+  {
+    Country: "China",
+    ISO2: "CN"
+  },
+  {
+    Country: "Colombia",
+    ISO2: "CO"
+  },
+  {
+    Country: "Cuba",
+    ISO2: "CU"
+  },
+  {
+    Country: "Czech Republic",
+    ISO2: "CZ"
+  },
+  {
+    Country: "Germany",
+    ISO2: "DE"
+  },
+  {
+    Country: "Egypt",
+    ISO2: "EG"
+  },
+  {
+    Country: "France",
+    ISO2: "FR"
+  },
+  {
+    Country: "United Kingdom",
+    ISO2: "GB"
+  },
+  {
+    Country: "Greece",
+    ISO2: "GR"
+  },
+  {
+    Country: "Hong Kong, SAR China",
+    ISO2: "HK"
+  },
+  {
+    Country: "Hungary",
+    ISO2: "HU"
+  },
+  {
+    Country: "Indonesia",
+    ISO2: "ID"
+  },
+  {
+    Country: "Ireland",
+    ISO2: "IE"
+  },
+  {
+    Country: "Israel",
+    ISO2: "IL"
+  },
+  {
+    Country: "India",
+    ISO2: "IN"
+  },
+  {
+    Country: "Italy",
+    ISO2: "IT"
+  },
+  {
+    Country: "Japan",
+    ISO2: "JP"
+  },
+  {
+    Country: "Korea (South)",
+    ISO2: "KR"
+  },
+  {
+    Country: "Lithuania",
+    ISO2: "LT"
+  },
+  {
+    Country: "Latvia",
+    ISO2: "LV"
+  },
+  {
+    Country: "Morocco",
+    ISO2: "MA"
+  },
+  {
+    Country: "Mexico",
+    ISO2: "MX"
+  },
+  {
+    Country: "Malaysia",
+    ISO2: "MY"
+  },
+  {
+    Country: "Nigeria",
+    ISO2: "NG"
+  },
+  {
+    Country: "Netherlands",
+    ISO2: "NL"
+  },
+  {
+    Country: "Norway",
+    ISO2: "NO"
+  },
+  {
+    Country: "New Zealand",
+    ISO2: "NZ"
+  },
+  {
+    Country: "Philippines",
+    ISO2: "PH"
+  },
+  {
+    Country: "Poland",
+    ISO2: "PL"
+  },
+  {
+    Country: "Portugal",
+    ISO2: "PT"
+  },
+  {
+    Country: "Romania",
+    ISO2: "RO"
+  },
+  {
+    Country: "Serbia",
+    ISO2: "RS"
+  },
+  {
+    Country: "Russian Federation",
+    ISO2: "RU"
+  },
+  {
+    Country: "Saudi Arabia",
+    ISO2: "SA"
+  },
+  {
+    Country: "Sweden",
+    ISO2: "SE"
+  },
+  {
+    Country: "Singapore",
+    ISO2: "SG"
+  },
+  {
+    Country: "Slovenia",
+    ISO2: "SI"
+  },
+  {
+    Country: "Slovakia",
+    ISO2: "SK"
+  },
+  {
+    Country: "Thailand",
+    ISO2: "TH"
+  },
+  {
+    Country: "Turkey",
+    ISO2: "TR"
+  },
+  {
+    Country: "Taiwan, Republic of China",
+    ISO2: "TW"
+  },
+  {
+    Country: "Ukraine",
+    ISO2: "UA"
+  },
+  {
+    Country: "United States of America",
+    ISO2: "US"
+  },
+  {
+    Country: "Venezuela (Bolivarian Republic)",
+    ISO2: "VE"
+  },
+  {
+    Country: "South Africa",
+    ISO2: "ZA"
+  }
+];
