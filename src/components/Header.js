fe7b2d5049b1c868cac2f931a504d01af954f19b
AlfredoCU <alfredo_11_cu@hotmail.com>
2021-03-04 00:04:41 -0600

    Iniciando proyecto.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-const Header = () => {
-  return (
-    <div>
-      <p>Header</p>
-    </div>
-  );
-};
+import { memo } from "react";
 
-export { Header };
+const Header = () => (
+  <header className="bg-green-500 m-auto p-5">
+    <h1 className="text-center text-white text-2xl font-medium">
+      BUSCADOR DE NOTICIAS
+    </h1>
+  </header>
+);
+
+const HeaderMemo = memo(Header);
+export { HeaderMemo as Header };
